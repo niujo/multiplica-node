e22d9f7ea2aef922bae98a3590d97a45ae3d37f0
claudio <cla.cardenasd@gmail.com>
2019-07-16 10:09:58 -0400

    commit inicial

--- app.js
+++ app.js
@@ -1,30 +1,10 @@
-const argv = require('yargs')
-               .command('listar','Impreme en consola la tabla de multiplicar',{
-                    base:{
-                        demand: true,
-                        alias:  'b'
-                    },
-                    limite:{
-                        alias: 'l',
-                        default: 10
-                    }
-                })
-                .command('crear','Genera un archivo con la tabla de multiplicar',{
-                    base:{
-                        demand: true,
-                        alias:  'b'
-                    },
-                    limite:{
-                        alias: 'l',
-                        default: 10
-                    }                    
-                })
-                .help()
-                .argv;
+const argv = require('./config/yargs').argv;
+const colors = require('colors');
 
 const {crearArchivo,listabla}  = require('./multiplicar/multiplicar');
 
 let comando = argv._[0];
+
 switch( comando ){
     case 'listar':
         listabla( argv.base,argv.limite);
@@ -40,10 +20,3 @@ switch( comando ){
         console.log('Comando no reconocido');    
 
 }
-/* let parametro = argv[2];
-
-let base = parametro.split('=')[1]
-
-crearArchivo(base)
-    .then(archivo=>console.log(`Archivo creado ${archivo}`))
-    .catch(e=>console.log(e)); */
--- multiplicar/multiplicar.js
+++ multiplicar/multiplicar.js
@@ -1,7 +1,9 @@
 const fs  = require('fs');
+const colors = require('colors');
 
 module.exports.crearArchivo = (base, limite = 10)=>{
-   
+    
+
     return new Promise ((resolve, reject)=>{
         if(!Number(base)){
             reject(`El valor introducido ${base}  no es un numero`);
@@ -26,6 +28,9 @@ module.exports.crearArchivo = (base, limite = 10)=>{
     });
 }
 module.exports.listabla = (base, limite = 10)=>{
+    console.log('============='.green);
+    console.log(`tabla de ${base}`.green);
+    console.log('============='.green);
 
     for(let i=1;i<=limite; i++) {
         console.log(`${base} * ${i} = ${base*i} \n`);
